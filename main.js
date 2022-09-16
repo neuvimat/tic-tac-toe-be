0b096e6375d2461b10c3e65429590b9ff56d676b
Matyáš Neuvirt <matyas.neuvirt@gmail.com>
2022-09-16 21:45:08 +0200

    hardcoded stuff for now

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const WebSocket = require('ws');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5001
 const wss = new WebSocket.Server({port: port});
 
 const GameBank = new (require('./GameBank'))()
